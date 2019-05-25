b1949803ef7ef7ccbf2c5cf7e4bfcd122882e2b7
Georgi Yanev <georgiyanev.gy@gmail.com>
2019-05-26 01:11:43 +0300

    Init project

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+const path = require(`path`)
+require(`dotenv`).config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+module.exports = {
+  /* Your site config here */
+}
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Map from '../components/Map'
+
+export default () => (
+  <div>
+    <h1>Stories</h1>
+    <Map />
+  </div>
+)
